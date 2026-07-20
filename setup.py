259b61e7bbe7a3a5ae6e54a7476a33445a6bb131
Kalze <benjamin@iakzs.dev>
2026-07-20 11:27:26 -0400

    Oops! Forgot to update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="madurify",
-    version="0.2.1",
+    version="0.2.2",
     description="Face swapping application using geometric transformations",
     author="iakzs",
     packages=find_packages(),
